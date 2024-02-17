4f3fad5b9065ff189fb3118561b53d10ba441de9
Deynner Alexander Sanabria Rojas <107587926+DeynnerASR@users.noreply.github.com>
2024-02-17 17:16:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,79 +1,59 @@
+import * as THREE from 'three'
 
-import * as THREE from 'three';
 
+
+// Camara
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+// Escena
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// Renderizador 
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-/*
-aspectosCamara 
-    fov: ,
-    aspect: tamaño (dimensiones)
-    near: Primer valor del rango de vista
-    far: Segundo valor del rango de vista 
+//Agregar el renderizador al html, como un canvas
+ document.body.appendChild(renderer.domElement);
+
+
+
+
+// PROPIEDADES PARA UN SOLIDO
+
+    // Geometria
+    const geometry = new THREE.BoxGeometry(1,1,1);
+
+    // Material, hay barios tipos de material
+    const material = new THREE.MeshPhongMaterial( { color: 0x00ff00 });
+
+    //Armando el solido
+    const cubo = new THREE.Mesh(geometry,material);
+
+    //Agregando el solido a la escena
+    scene.add(cubo)
+
+// La posicion de la camara siempre esta desajustada, toca ajustarla
+camera.position.z = 5;
+
+
+// Agregar iluminacion
+    const light = new THREE.AmbientLight(0xffffff,1)
     
-    (near,far) // este es el rango en el cual se los elementos 
-                  añadidos a la escena se van a mostrar.
-*/
+    scene.add(light)
 
 
-// Configurando la posicion inicial de la camara
-camera.position.z = 2;
+// Comenzar a animar
+function animate() {
+	requestAnimationFrame( animate );
+    
+    cubo.rotation.x += 0.01;
+	cubo.rotation.y += 0.01;
 
-// Creando todo para una geometria
-const dimensionesCuadro = {
-    ancho: 1,
-    alto: 1,
-    profundo: 1,
+	renderer.render( scene, camera );
 }
+animate();
 
-const geometry = new THREE.BoxGeometry(
-    dimensionesCuadro.ancho,
-    dimensionesCuadro.alto,
-    dimensionesCuadro.profundo
-)
 
-const material = new THREE.MeshPhongMaterial({color : 0x44aa88})
 
-const cubos = [];
-
-const cubo1 = new THREE.Mesh(geometry,material)
-const cubo2 = new THREE.Mesh(geometry,material)
-
-cubos.push(cubo1)
-cubos.push(cubo2);
-
-// Agrego las figuras a la escena
-scene.add(cubo1);
-scene.add(cubo2)
-cubo2.position.x = 2;
-
-//Empezamos a renderizar nuestra escena con nuestra camara
-renderer.render(scene, camera);
-
-function render(time) {
-  time *= 0.001;  // convert time to seconds
- 
-  cubos.forEach((figura)=>{
-    figura.rotation.x = time;
-    figura.rotation.y = time;  
-  })
-  
- 
-  renderer.render(scene, camera);
- 
-  requestAnimationFrame(render);
-}
-
-requestAnimationFrame(render);
-
-//Crando una iluminacion
-
-const color = 0xFFFFFF;
-const intensity = 1;
-const light = new THREE.DirectionalLight(color, intensity);
-light.position.set(-1, 2, 4);
-scene.add(light);
